77cc12a36c466bdd4436c43e02a9d7290590140d
RicardoGiron <juan.giron@cognits.co>
2021-07-15 16:19:04 -0600

    [ADD] Action refactor functionality

--- src/commands/fix.js
+++ src/commands/fix.js
@@ -42,6 +42,7 @@ class FixCommand extends Command {
   fixForm() {
     const {flags: {type, apath, channel, fpath, module}, args: {to, name}} = this.parse(FixCommand)
     const formPath = path.join(to, fpath, channel, module, name + '.sm')
+    const globalIDs = []
     this.log(`Will fix form ${name} in module: ${module}\n     in: ${formPath} `)
 
     if (!fs.existsSync(formPath)) {
@@ -49,16 +50,52 @@ class FixCommand extends Command {
     }
     // TODO add fix form actions
     // obtener lista de archivos .json de un form basado en formPath
+    const files = fs.readdirSync(formPath);
 
+    files.forEach( (element) => {
+      const jsonPath = path.join(formPath, element)
+      const ids = this.getActionsIDsFromFile(jsonPath)
 
-    // revisar cada archivo .json y obtener los actions ids si existen
+      globalIDs.push(...ids)
+    });
 
-    // para cada id encontrado agregarlos a un array global
-
-    // usando el array con los actions ids iterar cada uno y hacer un fixAction
-
-    //
+    globalIDs.forEach( (element) => {
+      const actionFilePath = path.join(to, apath, 'mobile/'+element+'.json')
+      //console.log("ACTION PATH :" + actionFilePath)
+      this.fixGlobalActions(actionFilePath, element)
+    });
   }
+
+  getActionsIDsFromFile(filePath){
+    let fileJsonObj = this.filesUtil.getJsonFromFile(filePath)
+    let fileActionsIDs = []
+  
+    for (let [key, value] of Object.entries(fileJsonObj)) {
+      if((typeof value == "string") && (value.substr(0, 3) == "AS_"))
+        fileActionsIDs.push(value)
+    }
+    
+    return fileActionsIDs
+  }
+
+  fixGlobalActions(path, name) {
+
+    this.log(`Will fix action \n  with name: ${name}\n   in: ${path}`)
+
+    // check if json file exists
+    if (!fs.existsSync(path)) throw new Error(`File does not exists: \n    ${path}`)
+
+    // load json file object
+    let fileJsonObj = this.filesUtil.getJsonFromFile(path)
+    const actionsInFile = fileJsonObj[name] && fileJsonObj[name].actions ? fileJsonObj[name].actions : []
+    // will try to fix actions
+    this.log(`Will try to fix (${actionsInFile.length}) actions`)
+    fileJsonObj[name].actions = actionsInFile.map(a => this.canChangeInvokeToSnippet(a) ? this.changeInvokeToSnippet(a) : a)
+
+    this.filesUtil.rewriteFile(path, fileJsonObj)
+  }
+
+
 
   getFormatFromType() {
     const {flags: {type}} = this.parse(FixCommand)
